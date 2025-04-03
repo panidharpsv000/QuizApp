12dcc08cc9ac95f4a1e3668357348a2cf7830b0c
panidharpvs00 <120071353+panidharpsv000@users.noreply.github.com>
2025-04-03 06:24:19 +0530

    Update UserRepo.java

--- backend/repository/UserRepo.java
+++ backend/repository/UserRepo.java
@@ -7,7 +7,9 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepo extends JpaRepository<User, Long>{
-	@Query("SELECT MAX(id) FROM User")
+	@Query("SELECT (id) FROM User WHERE mobile=:mobile AND password=:password")
+	public Long findId(String mobile,String password);
+	@Query("SELECT (id) FROM User ")
 	public Long findId();
 	public boolean existsByMobileAndPassword(String mobile,String password);
 
